95ca3dbf3cd1705300eb1675dd1025a597cb6f97
Martin Milata <mmilata@redhat.com>
2014-07-23 12:00:59 +0200

    Make client auth optional

--- app/models/abrt_report.rb
+++ app/models/abrt_report.rb
@@ -54,16 +54,22 @@ class AbrtReport < ActiveRecord::Base
 
   # XXX is the network communication acceptable in a model?
   def forward
-    # XXX what certificates will be used if communicating with e.g. customer portal?
-    # TODO only if https
     request_params = {
       :timeout => 60,
       :open_timeout => 10,
-      :ssl_client_cert  =>  OpenSSL::X509::Certificate.new(File.read(Setting[:abrt_server_ssl_certificate])),
-      :ssl_client_key   =>  OpenSSL::PKey::RSA.new(File.read(Setting[:abrt_server_ssl_priv_key])),
-      :ssl_ca_file      =>  Setting[:abrt_server_ssl_ca_file],
       :verify_ssl => Setting[:abrt_server_verify_ssl] ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
     }
+
+    if Setting[:abrt_server_ssl_ca_file] && !Setting[:abrt_server_ssl_ca_file].empty?
+      request_params[:ssl_ca_file] = Setting[:abrt_server_ssl_ca_file]
+    end
+
+    # XXX what certificates will be used if communicating with e.g. customer portal?
+    if Setting[:abrt_server_ssl_certificate] && !Setting[:abrt_server_ssl_certificate].empty? \
+       && Setting[:abrt_server_ssl_priv_key] && !Setting[:abrt_server_ssl_priv_key].empty?
+      request_params[:ssl_client_cert] = OpenSSL::X509::Certificate.new(File.read(Setting[:abrt_server_ssl_certificate]))
+      request_params[:ssl_client_key]  = OpenSSL::PKey::RSA.new(File.read(Setting[:abrt_server_ssl_priv_key]))
+    end
 
     resource = RestClient::Resource.new(Setting[:abrt_server_url], request_params)
     response = resource['reports/new/'].post({:file => json, :multipart => true}, :content_type => :json, :accept => :json)
