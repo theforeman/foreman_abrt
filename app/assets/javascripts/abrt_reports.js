2e95d543b0c476cd5e19fd89194d5a8dce77db70
Martin Milata <mmilata@redhat.com>
2015-01-08 21:39:17 +0100

    Basic auth: use Red Hat Access plugin credentials

--- app/assets/javascripts/abrt_reports.js
+++ app/assets/javascripts/abrt_reports.js
@@ -1,4 +1,5 @@
 $(function () {
+  // basic http auth modal dialog setup
   var button = $("#forward_auth_button")
   var dialog = $("#forward_auth")
 
@@ -9,5 +10,28 @@ $(function () {
 
   button.on('click', function () {
     dialog.modal('show')
+  });
+
+  // filling the dialog with redhat_access credentials
+  var token = localStorage.getItem('rhAuthToken');
+  var rhUser = '';
+  var rhPass = '';
+  if (token) {
+     try {
+        decoded = atob(token);
+        colonPos = decoded.indexOf(':');
+        if (colonPos != -1) {
+          rhUser = decoded.substring(0, colonPos);
+          rhPass = decoded.substring(colonPos+1);
+          $("#redhat_access_alert_login").hide();
+          $("#redhat_access_alert_use").show();
+        }
+     } catch(err) {}
+  }
+
+  $("#redhat_access_fill").on('click', function (e) {
+    e.preventDefault();
+    $("#username").val(rhUser);
+    $("#password").val(rhPass);
   });
 });
